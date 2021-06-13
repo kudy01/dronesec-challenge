6231fcff3df79701a8b9fbe8f8d44a624380ad1e
Kushal Jaiswal <jaiswalk@student.unimelb.edu.au>
2021-06-13 12:03:09 +0530

    Data from api added to UI with proper error handling

--- src/pages/Dashboard/dashboard.js
+++ src/pages/Dashboard/dashboard.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
+import ReactTable from "react-table-6";
+import "react-table-6/react-table.css";
 
 const Dashboard = () => {
   const [movies, getMovies] = useState([]);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const getStarWarsMovies = () => {
     fetch("https://swapi.dev/api/films")
       .then((response) => response.json())
       .then((data) => {
         getMovies(data.results);
+      })
+      .catch((e) => {
+        setErrorMessage(e.message);
+        setError(true);
       });
   };
 
@@ -15,10 +23,40 @@ const Dashboard = () => {
     getStarWarsMovies();
   }, []);
 
+  const columns = [
+    {
+      Header: "Name",
+      accessor: "title",
+    },
+    {
+      Header: "Director",
+      accessor: "director",
+    },
+    {
+      Header: "Producers",
+      accessor: "producer",
+    },
+    {
+      Header: "Opening Lines",
+      accessor: "opening_crawl",
+    },
+  ];
+
   return (
     <div>
-      <h1 className="white">List of Star Wars Movies</h1>
-      {movies && console.log(movies)}
+      <h1 className="white pt4 pb6">List of Star Wars Movies</h1>
+      {error ? (
+        <h3 className="white pa4">Something went wrong: {errorMessage}</h3>
+      ) : (
+        <ReactTable
+          data={movies}
+          columns={columns}
+          defaultPageSize={3}
+          pageSizeOptions={[3]}
+          style={{ background: "white" }}
+          className="-striped -highlight"
+        />
+      )}
     </div>
   );
 };
