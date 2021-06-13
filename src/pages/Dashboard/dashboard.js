da51da63539ced0de3a139405cebf660301a36cf
Kushal Jaiswal <jaiswalk@student.unimelb.edu.au>
2021-06-13 11:52:55 +0530

    Basic API integration added

--- src/pages/Dashboard/dashboard.js
+++ src/pages/Dashboard/dashboard.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Dashboard = () => {
+  const [movies, getMovies] = useState([]);
+
+  const getStarWarsMovies = () => {
+    fetch("https://swapi.dev/api/films")
+      .then((response) => response.json())
+      .then((data) => {
+        getMovies(data.results);
+      });
+  };
+
+  useEffect(() => {
+    getStarWarsMovies();
+  }, []);
+
   return (
     <div>
-      <h1 className="white">helooooo</h1>
+      <h1 className="white">List of Star Wars Movies</h1>
+      {movies && console.log(movies)}
     </div>
   );
 };
